455c30120248c63873a7705888c09fa697a35f39
김민지 <minji@gimminjiui-MacBookPro.local>
2020-01-08 05:54:44 +0900

    2.2 Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Platform } from "react-native";
 import { Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
@@ -6,18 +7,50 @@ import Movies from "./../screens/Movies";
 import Seach from "../screens/Seach";
 import Tv from "../screens/Tv";
 import { BG_COLOR } from "../constants/Colors";
+import TabBarIcon from "../components/TabBarlcon";
 
 const TabNavigator = createBottomTabNavigator(
   {
-    Moives: { screen: Movies },
-    TV: { screen: Tv },
-    Search: { screen: Seach }
+    Moives: {
+      screen: Movies,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+            // Platform OS가 ios 입니까? 만약에 ios 이면 ios전용 아이콘 드리고 아니라면 : 안드로이드 드리겠슴
+          />
+        )
+      }
+    },
+    TV: {
+      screen: Tv,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+          />
+        )
+      }
+    },
+    Search: {
+      screen: Seach,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-search" : "md-Search"}
+          />
+        )
+      }
+    }
   },
   {
     tabBarOptions: {
-      showLabel: false,
+      //showLabel: false,
       style: {
-        backgroundColor: BG_COLOR
+        backgroundColor: "white"
       }
     }
   }
